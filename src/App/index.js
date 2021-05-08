0c982b45b793d4af2c4e50e70a348a90bec21a4c
RodneyMac <rodneydavidmaciel@gmail.com>
2021-05-08 10:39:31 -0300

    Actualización Modal

---
+++ src/App/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Home from '../components/Home';
+import Log from '../components/Login';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import Table from '../components/Table/Table';
+import ModalEdit from '../components/Modal';
+
+function App() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/home">
+                    Barra de navegación
+                    <Home />
+                </Route>
+                <Route path="/login">
+                    <Log />
+                </Route>
+                <Route path="/table">
+                    <Table />
+                </Route>
+                <Route path="/modal">
+                    <ModalEdit/>
+                </Route>
+            </Switch>
+        </Router>
+    )
+}
+
+export default  App;
